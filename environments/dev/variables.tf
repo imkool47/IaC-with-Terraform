b59fdf60a0c55e0594ca2e1dde9d3f8cf95f6778
Mukul Kumar <mukul.kumar630@gmail.com>
2025-06-05 18:45:53 +0530

    add aws provider configuration and enhance aws_region variable with description and type

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,4 +1,7 @@
-variable "aws_region" {}
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
 variable "vpc_cidr" {}
 variable "public_subnet_cidrs" { type = list(string) }
 variable "azs" { type = list(string) }
