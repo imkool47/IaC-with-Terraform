36068b93de8441a9b42703bf53c2ec5f95220d48
Mukul Kumar <mukul.kumar630@gmail.com>
2025-06-05 14:57:34 +0530

    refactor module source paths to use relative paths

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,12 +1,12 @@
 module "network" {
-  source               = "./modules/network"
+  source               = "../../modules/network"
   vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs  = var.public_subnet_cidrs
   azs                  = var.azs
 }
 
 module "ec2" {
-  source         = "./modules/ec2"
+  source         = "../../modules/ec2"
   ami            = var.ami
   instance_type  = var.instance_type
   subnet_id      = module.network.public_subnet_ids[0]
@@ -14,6 +14,6 @@ module "ec2" {
 }
 
 module "s3" {
-  source      = "./modules/s3"
+  source      = "../../modules/s3"
   bucket_name = var.bucket_name
 }
