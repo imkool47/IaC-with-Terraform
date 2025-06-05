b372c898567750e329bb5985216ee609ab83cb47
Mukul Kumar <mukul.kumar630@gmail.com>
2025-06-05 13:09:46 +0530

    update backend configuration to use Terraform Cloud; remove S3 backend block

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,17 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-remote-state-mukul"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-remote-state-mukul"
+#     key    = "dev/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+terraform { 
+  cloud { 
+    
+    organization = "imkool-aws-terraform" 
+
+    workspaces { 
+      name = "IaC-with-Terraform" 
+    } 
+  } 
 }
